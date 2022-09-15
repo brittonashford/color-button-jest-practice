8c597c156920aac799245a84a774e71e681698c3
Bret Kitchell <brittonashford@gmail.com>
2022-09-14 22:10:52 -0500

    color button test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button renders correct initial color', () => {
+    render(<App />);
+
+    //button exists, initial text
+    const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+    //initial color 
+    expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+    //click
+    fireEvent.click(colorButton);
+
+    //expect color to be blue
+    expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+    //button text --> 'Change to red'
+    expect(colorButton.textContent).toBe('Change to red');
 });
